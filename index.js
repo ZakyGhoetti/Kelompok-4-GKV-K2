1655c2168015ef863ed32afcb1efb437c00d2ee1
Zaky-G <lyx29ghoetti@apps.ipb.ac.id>
2024-06-14 22:45:29 +0700

    Final, connected with another page, beautify the looks

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const width = 1200
-const height = 1000
+const height = 400
 const csvDataDict = {};
+const csvDataDict2 = {}; //proccessing only
 let selectedyear = '2020'
 
 const svg = d3.select('#map').append('svg').attr('width', width).attr('height', height);
 
-const projection = d3.geoMercator().scale(1300).center([120, -7]).translate([width / 1.8, height / 3]);
+const projection = d3.geoMercator().scale(1300).center([120, 2]).translate([width / 1.8, height / 3]);
 const path = d3.geoPath().projection(projection);
 
 const div = d3.select("#tooltip");
@@ -14,15 +15,20 @@ let indo;
 
 Promise.all([
     d3.json('provinces-simplified-topo.json'),
-    d3.dsv(';', 'data-stunting-indonesia.csv')
+    d3.dsv(';', 'data-stunting-indonesia.csv'),
+    d3.dsv(';', 'data-stunting-indonesia_for_counting.csv')
 ]).then(data => {
 
-    const [provincesData, stuntingData] = data;
+    const [provincesData, stuntingData, stuntingData2] = data;
     const provinces = topojson.feature(provincesData, provincesData.objects.provinces).features;
     
     stuntingData.forEach(row => {
         csvDataDict[row.Provinsi] = row;
     });
+
+    stuntingData2.forEach(row => {
+      csvDataDict2[row.Provinsi] = row;
+  });
 
     function mouseover(d) {
         d3.select(this)
@@ -44,13 +50,19 @@ Promise.all([
         div.style("opacity", 0);
     }
 
+    function click(d) {
+        const provinceName = d.properties.provinsi;
+        showBarChart(provinceName);
+    }
+
     indo = svg.selectAll('path')
     .data(provinces)
     .enter().append('path')
     .attr('class', 'province')
     .attr('d', path)
     .on("mouseover", mouseover)
-    .on("mouseout", mouseout);
+    .on("mouseout", mouseout)
+    .on("click", click);
 
     indo.on("mousemove", function(d) {
         div.style("opacity", 0.9);
@@ -82,7 +94,7 @@ Promise.all([
 function updateMapColors() {
   indo.style("fill", d => {
       const provinceName = d.properties.provinsi;
-      const additionalInfo = csvDataDict[provinceName];
+      const additionalInfo = csvDataDict2[provinceName];
       const dat = parseFloat(additionalInfo[selectedyear]);
       console.log(selectedyear);
       console.log(provinceName + " " + dat);
@@ -106,10 +118,67 @@ function updateMapColors() {
 
 function setYear(year) {
     selectedyear = year;
-    d3.select('#judul').text("Data Stunting Indonesia " + selectedyear);
+    d3.select('#judul').text("Data Stunting Indonesia (" + selectedyear + ")");
     if (typeof updateMapColors === "function") {
       updateMapColors(); // Call the update function
   } else {
       console.log("updateMapColors is not defined");
   }
+}
+
+function showBarChart(provinceName){
+  const provincedata = csvDataDict2[provinceName];
+  const tahun = ['2020', '2021', '2022', '2023'];
+  const data = tahun.map(th => ({
+    Tahun: th,
+    nilai: parseFloat(provincedata[th]) 
+  }));
+
+  const margin = {top: 30, right: 30, bottom: 40, left: 40};
+  const chartWidth = 300 - margin.left - margin.right;
+  const chartHeight = 200 - margin.top - margin.bottom;
+  const x = d3.scaleBand()
+      .domain(data.map(d => d.Tahun))
+      .range([0, chartWidth])
+      .padding(0.1);
+
+  const y = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d.nilai)])
+      .range([chartHeight, 0]);
+
+      d3.select("#barchart").selectAll("*").remove(); // Clear any existing chart
+      
+      const svg = d3.select("#barchart")
+      .append("svg")
+      .attr("width", chartWidth + margin.left + margin.right)
+      .attr("height", chartHeight + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  svg.append("g")
+      .selectAll("rect")
+      .data(data)
+      .enter().append("rect")
+      .attr("x", d => x(d.Tahun))
+      .attr("y", d => parseFloat(y(d.nilai)))
+      .attr("width", x.bandwidth())
+      .attr("height", d => parseFloat(chartHeight) - parseFloat(y(d.nilai)))
+      .attr("fill", "#69b3a2");
+
+  svg.append("g")
+      .attr("class", "x-axis")
+      .attr("transform", "translate(0," + chartHeight + ")")
+      .call(d3.axisBottom(x));
+
+  svg.append("g")
+      .attr("class", "y-axis")
+      .call(d3.axisLeft(y).ticks(5).tickFormat(d3.format(".2f")));
+
+  svg.append("text")
+      .attr("x", (chartWidth / 2))             
+      .attr("y", 0 - (margin.top / 2))
+      .attr("text-anchor", "middle")  
+      .style("font-size", "16px") 
+      .style("text-decoration", "underline")  
+      .text(provinceName);
 }
